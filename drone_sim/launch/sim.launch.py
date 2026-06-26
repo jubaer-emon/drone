0e59f088e49b8306ba7d4a1eb74e06377396f76a
jubaer-emon <jubaer366@gmail.com>
2026-06-26 14:56:19 +0600

    add paths. add event handlers for  shutdown on Gazebo and PX4 exit

--- drone_sim/launch/sim.launch.py
+++ drone_sim/launch/sim.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, Shutdown
+from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node, SetParameter
 from launch.logging import get_logger
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -14,15 +15,29 @@ def generate_launch_description():
     pkg_dir = get_package_share_path('drone_sim')
 
     gz_resoure_path = ':'.join([
-        str(pkg_dir / 'worlds'),
-        str(pkg_dir / 'models'),
         os.environ.get('GZ_SIM_RESOURCE_PATH', ''),
+        str(dev_dir / 'PX4-Autopilot/Tools/simulation/gz/models'),
+        str(dev_dir / 'PX4-Autopilot/Tools/simulation/gz/worlds'),
+        str(pkg_dir / 'models'),
+        str(pkg_dir / 'worlds'),
     ])
- 
+
+    px4_gz_plugins = str(dev_dir 
+                         / 'PX4-Autopilot/build/px4_sitl_default/src/modules/simulation/gz_plugins')
+    
+    gz_server_config = str(dev_dir 
+                           / 'PX4-Autopilot/src/modules/simulation/gz_bridge/server.config')
+
     gz_sim = ExecuteProcess(
-        cmd=['gz', 'sim', '-r', 'simple_room.sdf'],
+        cmd=['gz', 'sim', '-r', 'default.sdf', '--verbose', '4'],
         additional_env= {
+            'PX4_GZ_WORLD': 'default',
+            'PX4_GZ_PLUGINS': px4_gz_plugins,
+            'GZ_SIM_SYSTEM_PLUGIN_PATH': px4_gz_plugins,
             'GZ_SIM_RESOURCE_PATH': gz_resoure_path,
+            'GZ_SIM_SERVER_CONFIG_PATH': gz_server_config,
+            # 'DRI_PRIME': '1',
+            # 'HEADLESS': '1',
         },
         output='screen',
     )
@@ -31,11 +46,10 @@ def generate_launch_description():
         cmd=['./build/px4_sitl_default/bin/px4'],
         cwd=str(dev_dir / 'PX4-Autopilot'),
         additional_env= {
-            'PX4_GZ_WORLD': 'simple_room',
+            'PX4_GZ_WORLD': 'default',
             'PX4_SYS_AUTOSTART': '4001',
-            'PX4_SIM_MODEL': 'x500_flow_lidar_2d',
+            'PX4_SIM_MODEL': 'x500_flow',
             'PX4_GZ_STANDALONE': '1',
-            # 'HEADLESS': '1',
         },
         output='screen',
     )
@@ -55,6 +69,13 @@ def generate_launch_description():
         output='screen',
     )
 
+    # OPTIONAL: Uncomment this node if you ever want to bridge cameras cleanly
+    # ros_gz_image_bridge = Node(
+    #     package='ros_gz_image',
+    #     executable='image_bridge',
+    #     arguments=['/camera/image_raw'],
+    #     output='screen',
+    # )
 
     common_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -72,6 +93,19 @@ def generate_launch_description():
         )
     )
 
+    shutdown_on_gz_exit = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=gz_sim,
+            on_exit=[Shutdown(reason='Gazebo Sim exited or crashed.')]
+        )
+    )
+    shutdown_on_px4_exit = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=px4_sitl,
+            on_exit=[Shutdown(reason='PX4 Autopilot SITL exited or crashed.')]
+        )
+    )
+
     return LaunchDescription([
         SetParameter(name='use_sim_time', value=True),
 
@@ -80,5 +114,8 @@ def generate_launch_description():
         
         px4_sitl,
         common_launch,
-        gcs,
+        # gcs,
+
+        shutdown_on_gz_exit,
+        shutdown_on_px4_exit,
     ])
